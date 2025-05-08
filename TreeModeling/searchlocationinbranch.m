7357b5f73e6c1abc711080636be5d83a46fd7b49
Smart3DLiDAR <15870690895@163.com>
2025-05-08 11:01:47 +0800

    Add files via upload

---
+++ TreeModeling/searchlocationinbranch.m
@@ -0,0 +1,41 @@
+function [curbranch,curlevel,lastevel,curlevelid] = searchlocationinbranch(curid,finnalBranch)
+%%----给枝干分层级--------------------------------------------------------------------------------
+%%input 
+%  curid           当前节点id
+%  finnalBranch    枝干
+%%output 
+%  curbranch       当前枝干
+%  curlevel        当前层级
+%  lastevel        上一层级
+%  curlevelid      当前枝干id
+%--------------------------------------------------------------------------------------------------
+for i = 1:length(finnalBranch)
+    for j = 1:length(finnalBranch{i,1})
+        [row,~] = find(finnalBranch{i,1}{j,1} == curid);
+        if ~isempty(row)
+            curbranch = finnalBranch{i,1}{j,1};
+            curlevelid = [i j];
+            level = finnalBranch{i,1};
+            curlevel = [];
+            for k = 1:length(level)
+                Temp = level{k,1};
+                curlevel = [curlevel;Temp];
+            end
+        end
+    end
+end
+clear i j 
+
+if curlevelid(1) ~= 1
+    last = finnalBranch{curlevelid(1)-1,1};
+    lastevel = [];
+    for j = 1:length(last)
+        Temp = last{j,1};
+        lastevel = [lastevel;Temp];
+    end
+else
+    lastevel = [];
+end
+%curblength = finnalBranchL{curbranchid(1),1}(curbranchid(2),1);
+%curblength = finnalD(curid);
+end
