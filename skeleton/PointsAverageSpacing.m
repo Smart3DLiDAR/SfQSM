b23bb6e7de7147e0f9c65de1c31cc6fd01112594
Smart3DLiDAR <15870690895@163.com>
2025-05-08 09:31:37 +0800

    Add files via upload

--- skeleton/PointsAverageSpacing.m
+++ skeleton/PointsAverageSpacing.m
@@ -1,7 +1,7 @@
 function [averageDistance] = PointsAverageSpacing(data)
 kdtree = KDTreeSearcher(data);
 [~,distances]=knnsearch(kdtree,data,'K',2);
-totalDistance = sum(sum(distances,'omitnan'));
+totalDistance = sum(sum(distances));
 averageDistance = totalDistance/size(data,1);
 fprintf("平均点间距?%f\n",averageDistance)
 end
