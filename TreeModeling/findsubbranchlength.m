b0e25b01a81acb9c21866a8cffd2b26d1806b650
Smart3DLiDAR <15870690895@163.com>
2025-05-08 10:12:03 +0800

    Add files via upload

---
+++ TreeModeling/findsubbranchlength.m
@@ -0,0 +1,33 @@
+function cursontreelength = findsubbranchlength(spls,finnalBranch,finnalGp,newPid)
+%%----Calculate the length of the sub-branches-----------------------------
+%---input -----------------------------------------------------------------
+%  spls : Skeleton points
+%  finnalBranch : Branch
+%  finnalGp : Skeleton graph
+%  newPid : The parent node of Skeleton points
+%---output ----------------------------------------------------------------
+%  cursontreelength : the length of the sub-branches
+%% 
+cursontreelength = zeros(1,size(spls,1));
+for j = 1:size(spls,1)
+    curid = j;
+    for i = 1:length(finnalBranch)
+        for k = 1:length(finnalBranch{i,1})
+            curbranch = finnalBranch{i,1}{k,1};
+            roi = intersect(curbranch,curid);
+            if ~isempty(roi)
+                [~,dis] = shortestpath(finnalGp,curid,curbranch(end));
+            end
+        end
+    end
+    cursontreelength(j) = dis;
+end
+
+[~,col] = find(cursontreelength==0);
+for i = 1:length(col)
+    curnormal = col(i);
+    pid = newPid(curnormal);
+    cursontreelength(curnormal) = cursontreelength(pid);
+end
+
+end
