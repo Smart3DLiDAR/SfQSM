da0363c9256b8c606ffaef17a5e497a4a819ac38
Smart3DLiDAR <15870690895@163.com>
2025-05-08 13:18:49 +0800

    Update findbranch.m

--- tree_graph/findbranch.m
+++ tree_graph/findbranch.m
@@ -1,16 +1,16 @@
 function [curbranch,Candidate] = findbranch(seedid,Cid,pli,Candidate)
-curbranch = zeros(0,1); %储存当前访问的点
+curbranch = zeros(0,1); 
 for i = 1:inf
-    %id = knnsearch(spls,seedpoint);% 找到当前点在当前枝干骨架点中的位置
+    %id = knnsearch(spls,seedpoint);
     id = seedid;
-    %[~,col] = find(adj(id,:)==1);% 找到id在原骨架中的连接情况搜索下一个需要调整的点
+    %[~,col] = find(adj(id,:)==1);
     col = Cid(id,:);
-    col(:,all(col==0,1))=[];%找到id在原骨架中下一个可能需要调整的点
+    col(:,all(col==0,1))=[];
     newroi = intersect(curbranch,id);
     if ~isempty(newroi)
         break
     end
-    curbranch(end+1,1) = id;%储存当前处理的点索引         
+    curbranch(end+1,1) = id;       
     
     if length(col)==1
         seedid = col;
@@ -20,11 +20,11 @@ for i = 1:inf
             [row,~] = find(pli(:,1) == col(j));
             add = [add,row];
         end
-        [~,maxid] = max(pli(add,2)); %比较连接点各自在路径中出现的频数 出现次数多的为主要的分支
+        [~,maxid] = max(pli(add,2)); 
         seedid = col(maxid(1));
         otherid = setdiff([1:length(col)],col(maxid(1)));
         for k = 1:length(otherid)
-            Candidate(col(otherid(k))) = 1;%遇到分叉处的其他分支点储存作为新的起始点的候选点
+            Candidate(col(otherid(k))) = 1;
         end
     else
         break
