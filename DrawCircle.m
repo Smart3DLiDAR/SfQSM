e4c69d91322d190887bfadafd12f6d2b34e2e946
Smart3DLiDAR <15870690895@163.com>
2025-05-08 09:03:28 +0800

    Delete DrawCircle.m

--- DrawCircle.m
+++
@@ -1,9 +0,0 @@
-function DrawCircle(center,r,z)
-%ªÊ÷∆‘≤
-theta = 0:pi/100:2*pi;
-x=r*cos(theta)+center(1); 
-y=r*sin(theta)+center(2);
-tz = zeros(length(x),1);
-tz(:) = z;
-plot3(x,y,tz,'g-','LineWidth',2)
-end
