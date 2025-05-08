9a1e95589944ec3bca7fd4965af44d50c6082a7d
Smart3DLiDAR <15870690895@163.com>
2025-05-08 10:09:32 +0800

    Add files via upload

---
+++ TreeModeling/findabnormalRadius.m
@@ -0,0 +1,73 @@
+function [normallabel,link,Radius,TH] = findabnormalRadius(Radius,finnalBranch,rootid,newCid,newPid,subbranchlength,link)
+%%
+normallabel = zeros(length(Radius),1);
+
+for i = 1:length(finnalBranch)
+    for j = 1:length(finnalBranch{i,1})
+        curbranch = finnalBranch{i,1}{j,1};
+        for k = 1:length(curbranch)
+            id = curbranch(k);
+            cursubbranchL = subbranchlength(id);
+            
+            if id ~= rootid
+                pid = newPid(id,1);%
+                cursubbranchLP = subbranchlength(pid);
+                % compared the sub-branch length
+                minR = Radius(pid)*((cursubbranchL/cursubbranchLP)^1.5);
+                if Radius(id)<= Radius(pid) && Radius(id) >= minR
+                    normallabel(id,:)=1;% 1：normal；0：abnormal
+                end
+            else
+                normallabel(id,1) = 1;
+            end
+        end
+    end
+end
+
+for i = 1:size(link,1)
+    id1 = link(i,2);
+    cursubbranchL = subbranchlength(id1);
+    
+    id2 = link(i,1);
+    cursubbranchLP = subbranchlength(id2);
+    
+    son = newCid(id2,:);
+    son(son==0) = [];
+
+    th1 = log(Radius(id1)/Radius(id2));
+    th2 = log(cursubbranchL/cursubbranchLP);
+
+    % If the current point is the only child of the parent node
+    % compared the sub-branch length
+    if th1~=0 && th2 ~=0
+        th = th1/th2;
+    else
+        th = 1.5;
+    end
+    
+    if normallabel(id1)==1 && Radius(id2) >= Radius(id1)
+        link(i,3:6) = [Radius(id2) Radius(id1) 1 th];
+    else
+        link(i,3:6) = [Radius(id2) Radius(id1) 0 th];
+    end
+end
+
+TH = cell(length(finnalBranch),1);
+for i = 1:length(finnalBranch)
+    tempth = {};
+    for j = 1:length(finnalBranch{i,1})
+        curbranch = finnalBranch{i,1}{j,1};
+        temp = [];
+        for k = 1:length(curbranch)
+            id = curbranch(k);%当前子节点
+            if id ~= rootid
+                [row,~] = find(link(:,2)==id);
+                temp(end+1,:) = link(row,5:6);
+            end
+        end
+        tempth{j,1} = temp;
+    end
+    TH{i,1} = tempth;
+end
+
+end
---
+++ TreeModeling/fixabnormalRadius.m
@@ -0,0 +1,117 @@
+function [newRadius,link,BranchRadius] = fixabnormalRadius(Radius,finnalBranch,link,rootid,newCid,newPid,subbranchlength,normallabel,TH)
+%%
+newRadius = zeros(length(Radius),3);
+BranchRadius = {};
+for i = 1:length(finnalBranch)
+    curTH = cell2mat(TH{i,:});
+    [row,~] = find(curTH(:,1)==1 & curTH(:,2)>=0);
+    if ~isempty(row)
+        curth(:,1) = mean(curTH(row,2),"omitnan");% Mean value
+        curth(:,2) = min(curTH(row,2),[],'all',"omitnan");% Minimum value
+    else
+        curth = [1.5  1/2.49];
+    end
+
+    for j = 1:length(finnalBranch{i,1})
+        curbranch = finnalBranch{i,1}{j,1};
+        for k = 1:length(curbranch)
+            id = curbranch(k);
+            curson = newCid(id,:);
+            curson(curson==0) = [];
+            cursubbranchL = subbranchlength(id);
+            
+            if id ~= curbranch(1)
+                pid = newPid(id,1);
+                cursubbranchLP = subbranchlength(pid);
+                
+                son = newCid(pid,:);
+                son(son==0) = [];
+                if normallabel(id) == 0
+                    % If the radius of this node is abnormal 
+                    % it needs to be corrected
+                    if length(son)==1
+                        th = curth(1);% Mean value
+                    else
+                        th = curth(2);% Minimum value
+                    end
+                    tempth = cursubbranchL/cursubbranchLP;
+
+                    tempR = newRadius(pid)*(tempth^th);
+                    newRadius(id,:) = [tempR i th];
+                    
+                    [curfix1,~] = find(link(:,2)==id);
+                    [curfix2,~] = find(link(:,1)==id);
+                    link(curfix1,4:6) = [tempR 1 th];
+                    link(curfix2,3) = tempR;
+
+                    TH{i,1}{j,1}(k-1,:) = [1 th];
+                else
+                    [ind,~] = find(link(:,2)==id);
+                    tempR = Radius(id);
+                    newRadius(id,:) = [tempR i link(ind,6)];
+                end
+                
+                % 找到该节点的子节点，判断其是否正确
+                if id ~= curbranch(end)
+                    cid = curbranch(k+1);
+                    [sonrow,~] = find(link(:,2) == cid);
+                    Lc = subbranchlength(cid);
+                    minR = tempR*((Lc/cursubbranchL)^curth(2));
+                    newth = (log(Radius(cid)/tempR))/(log(Lc/cursubbranchL));
+                    if Radius(cid)<= tempR && Radius(cid) >= minR*0.75%在合理范围内正常
+                        link(sonrow,5:6) = [1 newth];
+                        normallabel(cid) = 1;
+                    else
+                        link(sonrow,5) = 0;
+                        normallabel(cid) = 0;
+                    end
+                end
+            elseif id == curbranch(1) && id == rootid
+                tempR = Radius(rootid);
+                newRadius(id,:) = [tempR i 0];
+            else
+                pid = newPid(id,1);%当前子节点
+                cursubbranchLP = subbranchlength(pid);
+                tempth = cursubbranchL/cursubbranchLP;
+                if Radius(id)<= newRadius(pid) 
+                    [ind,~] = find(link(:,2)==id);
+                    tempR = Radius(id);
+                    newRadius(id,:) = [tempR i link(ind,6)];
+                else    
+                    tempR = newRadius(pid)*(tempth^curth(2));
+                    newRadius(id,:) = [tempR i curth(2)];
+                    [curfix1,~] = find(link(:,2)==id);
+                    [curfix2,~] = find(link(:,1)==id);
+                    link(curfix1,4:6) = [tempR 1 curth(2)];
+                    link(curfix2,3) = tempR;
+
+                    TH{i,1}{j,1}(1,:) = [1 curth(2)];
+                end
+
+                % 找到所有子圆柱 判断其是否正确
+                if id ~= curbranch(end)
+                    cid = curbranch(k+1);
+                    [sonrow,~] = find(link(:,2) == cid);
+                    Lc = subbranchlength(cid);
+                    minR = tempR*((Lc/cursubbranchL)^curth(2));
+                    newth = (log(Radius(cid)/tempR))/(log(Lc/cursubbranchL));
+                    if Radius(cid)<= tempR && Radius(cid) >= minR*0.75
+                        link(sonrow,5:6) = [1 newth];
+                        normallabel(cid) = 1;
+                    else
+                        link(sonrow,5) = 0;
+                        normallabel(cid) = 0;
+                    end
+                end
+            end
+            normallabel(id) = 1;
+            if tempR<0.001
+                tempR = 0.001;
+            end
+            BranchRadius{i,1}{j,1}(k,1) = tempR;
+        end
+    end
+end
+
+newRadius(newRadius(:,1)<0.001,:) = 0.001;
+end
